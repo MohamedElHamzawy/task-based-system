c464204d70b4dc4794e5e62b52f76ecd8138dfb7
Mohamed ElHamzawy <mohamed.elhamzawy.backend@gmail.com>
2023-08-15 14:32:42 +0300

    add login

---
+++ server/modules/login/login.controller.js
@@ -0,0 +1,21 @@
+const userModel = require("../../DB/user.model");
+const jwt = require("jsonwebtoken");
+
+const userLogin = async (req,res,next) => {
+    const {userName, password} = req.body;
+    const tryGetUser = await userModel.findOne({username: userName});
+    if (tryGetUser) {
+        if (tryGetUser.password === password) {
+            const token = jwt.sign({id: tryGetUser._id}, "tb2023");
+            res.json({
+                message: "User logged in successfully",
+                token,
+                user: tryGetUser
+            });
+        } else {
+            res.json({error: "Password is incorrect"});
+        }
+    } else {
+        res.json({error: "User doesn't exist on system"});
+    }
+}
