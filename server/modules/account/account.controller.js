f5bbaa4d518523ccbb88d0a6da39eba0bfa16151
Mohamed ElHamzawy <mohamed.elhamzawy.backend@gmail.com>
2023-08-27 18:41:18 +0300

    added transaction in get account response

--- server/modules/account/account.controller.js
+++ server/modules/account/account.controller.js
@@ -1,4 +1,5 @@
 const accountModel = require("../../DB/account.model");
+const transactionModel = require("../../DB/transaction.model");
 const HttpError = require("../../common/httpError");
 
 const getAllAccounts = async (req,res,next) => {
@@ -10,7 +11,8 @@ const getAccount = async (req,res,next) => {
     const accountID = req.params.id;
     const thisAccount = await accountModel.findById({_id: accountID});
     if (thisAccount) {
-        res.json({account: thisAccount});
+        const transactions = await transactionModel.find({account_id: thisAccount._id});
+        res.json({account: thisAccount, transactions: transactions});
     } else {
         return next(new HttpError("This account doesn't exist on system", 400));
     }
