9c5716ae49760662eecadafbec8171cf4462396c
Mohamed ElHamzawy <mohamed.elhamzawy.backend@gmail.com>
2023-09-12 19:37:16 +0300

    added status

--- server/modules/task/task.controller.js
+++ server/modules/task/task.controller.js
@@ -201,6 +201,11 @@ const partialUpdateTask = async (req,res,next) => {
             const date = new Date();
             await new noteModel({content: `${req.user.fullname} has set task to be cancelled in ${date}`, user_id: req.user._id, task_id: taskID}).save();
             res.json({msg:"Task set to cancelled successfully"});
+        } else if (currentStatus.slug == "edit") {
+            await taskModel.findByIdAndUpdate({_id: taskID}, {taskStatus: statusID});
+            const date = new Date();
+            await new noteModel({content: `${req.user.fullname} has set task to need edit in ${date}`, user_id: req.user._id, task_id: taskID}).save();
+            res.json({msg:"Task set to edit successfully"});
         } else {
             return next(new HttpError("You are not authorized to make this edit", 401));
         }
