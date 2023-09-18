f2a262b6b070611cbf16a6d7721dd2eb9f720187
Mohamed ElHamzawy <mohamed.elhamzawy.backend@gmail.com>
2023-09-18 20:46:37 +0300

    made filter route post request

--- server/modules/client/client.routes.js
+++ server/modules/client/client.routes.js
@@ -9,7 +9,7 @@ const router = require("express").Router();
 
 router.get("/", getAllClients);
 router.get("/:id", getClient);
-router.get("/sort/filter/", filterSortedClients);
+router.post("/sort/filter/", filterSortedClients);
 router.post("/", createClient);
 router.post("/:id", updateClient);
 router.delete("/:id", deleteClient);
