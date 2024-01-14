3d3daaa14bd88b1bc60eff60f048567ca1556780
Ziad Gaafar <ziadgaafar@protonmail.com>
2024-01-14 14:51:59 +0200

    Add notification icon to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { GrSearch } from "react-icons/gr";
 import { useNavigate } from "react-router";
+import { IoIosNotificationsOutline } from "react-icons/io";
 
 const Header = () => {
   const name = JSON.parse(localStorage.getItem("AdminName"));
@@ -14,7 +15,7 @@ const Header = () => {
       >
         Smarteduservices
       </h1>
-      <div className="w-full flex items-center justify-between">
+      <div className="w-full flex items-center justify-between space-x-6">
         <div className="relative flex-1 rounded-md">
           <input
             className="h-10 w-1/3 px-8 border-none ring-0"
@@ -25,9 +26,15 @@ const Header = () => {
           />
           <GrSearch className="absolute top-3 left-2" />
         </div>
+        <div className="relative cursor-pointer rounded-full transition-all active:scale-95">
+          <div className="select-none animate-bounce absolute top-0 right-0.5  bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
+            2
+          </div>
+          <IoIosNotificationsOutline className="w-8 h-8" />
+        </div>
         <div className="flex items-center space-x-4">
-          <div className="w-10 h-10 bg-blue-100 rounded-full flex justify-center items-center text-center">
-            {name.split("")[0].toUpperCase()}
+          <div className="w-10 h-10 bg-blue-100 rounded-full flex justify-center items-center text-center capitalize">
+            {name.charAt(0)}
           </div>
           <div className="text-lg text-center">
             <p className="m-0 p-0 text-base font-">{name}</p>
