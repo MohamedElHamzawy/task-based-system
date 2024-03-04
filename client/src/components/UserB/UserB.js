7884834f43f80a830fd569e856b8384772070ee5
Ziad Gaafar <ziadgaafar@protonmail.com>
2024-03-04 09:39:16 +0200

    Refactor UserB component structure and add Header

--- client/src/components/UserB/UserB.js
+++ client/src/components/UserB/UserB.js
@@ -1,50 +1,47 @@
-import React from 'react'
-import './UserB.css'
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import UserBSideBar from './UserBSideBar/UserBSideBar'
-import TaskDetails from './Pages/Tasks/TaskDetails'
-import FreeLancers from './Pages/FreeLancers/FreeLancers'
-import FreeLancerDetails from './Pages/FreeLancers/FreeLancerDetails'
-import AddFreeLancer from './Pages/FreeLancers/AddFreeLancer'
-import Settings from './Pages/Settings/Settings'
-import PendingTasks from './Pages/Tasks/PendingTasks';
-import YourTasks from './Pages/Tasks/YourTasks';
-import ChangePass from './Pages/Settings/ChangePass';
+import UserBSideBar from "./UserBSideBar/UserBSideBar";
+import TaskDetails from "./Pages/Tasks/TaskDetails";
+import FreeLancers from "./Pages/FreeLancers/FreeLancers";
+import FreeLancerDetails from "./Pages/FreeLancers/FreeLancerDetails";
+import AddFreeLancer from "./Pages/FreeLancers/AddFreeLancer";
+import Settings from "./Pages/Settings/Settings";
+import PendingTasks from "./Pages/Tasks/PendingTasks";
+import YourTasks from "./Pages/Tasks/YourTasks";
+import ChangePass from "./Pages/Settings/ChangePass";
 
-import Specialities from './Pages/Specialities/Specialities';
-import SpecialityDetails from './Pages/Specialities/specialityDetails';
-import AddSpeciality from './Pages/Specialities/addSpeciality';
-import Notifications from './Pages/Notifications/Notifications';
-
+import Specialities from "./Pages/Specialities/Specialities";
+import SpecialityDetails from "./Pages/Specialities/specialityDetails";
+import AddSpeciality from "./Pages/Specialities/addSpeciality";
+import Notifications from "./Pages/Notifications/Notifications";
+import Header from "../Header";
 
 const UserB = () => {
   return (
-    <div className='Admin w-100'>
-      <UserBSideBar className='sidebar' />
-
-      <div className='Admin-body '>
-        <Routes >     
-          <Route path="/task/:id" element={<TaskDetails />} />      
-          <Route path="/yourtasks" element={<YourTasks />} />      
-          <Route path="/" element={<PendingTasks />} />      
-          <Route path="/freelancers" element={<FreeLancers />} />
-          <Route path="/freelancer/:id" element={<FreeLancerDetails />} />
-          <Route path="/addfreelancer" element={<AddFreeLancer />} />
-
-          <Route path="/specialities" element={<Specialities />} />
-          <Route path="/speciality/:id" element={<SpecialityDetails />} />
-          <Route path="/addspeciality" element={<AddSpeciality />} />
-
-          <Route path="/notifications" element={<Notifications/>} />
-
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/changepass" element={<ChangePass />} />
-        </Routes>
+    <>
+      <Header />
+      <div className="ml-60 bg-[#F4F7FC]">
+        <UserBSideBar />
+        <div className="container pt-16">
+          <Routes>
+            <Route path="/task/:id" element={<TaskDetails />} />
+            <Route path="/yourtasks" element={<YourTasks />} />
+            <Route path="/" element={<PendingTasks />} />
+            <Route path="/freelancers" element={<FreeLancers />} />
+            <Route path="/freelancer/:id" element={<FreeLancerDetails />} />
+            <Route path="/addfreelancer" element={<AddFreeLancer />} />
+            <Route path="/specialities" element={<Specialities />} />
+            <Route path="/speciality/:id" element={<SpecialityDetails />} />
+            <Route path="/addspeciality" element={<AddSpeciality />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/changepass" element={<ChangePass />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default UserB
-
+export default UserB;
