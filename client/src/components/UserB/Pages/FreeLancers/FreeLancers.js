2f0841a75cbc5b162683758becad0a86a9664f76
Ziad Gaafar <ziadgaafar@protonmail.com>
2024-03-04 09:39:26 +0200

    Fix bug in login functionality

--- client/src/components/UserB/Pages/FreeLancers/FreeLancers.js
+++ client/src/components/UserB/Pages/FreeLancers/FreeLancers.js
@@ -1,11 +1,12 @@
-import "./FreeLancers.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import LoadingSpinner from "../../../../LoadingSpinner/LoadingSpinner";
 import { SiFreelancer } from "react-icons/si";
-import { FiFilter } from "react-icons/fi";
+import { FaFilter, FaPlus } from "react-icons/fa";
 import { RiDeleteBinFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
+import Filter from "../../../Filter";
 
 //search filter
 const getSearchFilter = (searchName, freeLancers) => {
@@ -91,163 +92,236 @@ const FreeLancers = () => {
     }
   };
 
+  const [filterOpen, setFilterOpen] = useState(false);
+  const navigate = useNavigate();
+
   return isLoading ? (
     <LoadingSpinner asOverlay />
   ) : (
-    <div className="row w-100 p-0 m-0 ">
-      <div className="col-12 row text-center system-head p-2">
-        <div className="col-12 col-sm-10 col-md-6 ">
-          <h1 className="logo text-white bg-danger p-2">Specialist Service</h1>
-        </div>
-        <h1 className="col-12  text-center fw-bold">System FreeLancers</h1>
-      </div>
-
-      <div className="row p-0 m-0 ">
-        <div className="col-8 col-md-4 p-2">
-          <input
-            type="name"
-            className="search p-2 w-100"
-            placeholder=" Search By Name"
-            value={searchName}
-            onChange={(e) => {
-              setSearchName(e.target.value);
-              setSpecialityFilterData(false);
-              setSearchFilterData(true);
-              setSpeciality("");
-            }}
-          />
-        </div>
-
-        <div className="col-12 col-md-5 text-secondary row p-2">
-          <label htmlFor="Speciality" className="m-2 col-5 text-end">
-            {" "}
-            <FiFilter className="" /> Filter:
-          </label>
-          <select
-            id="speciality"
-            name="speciality"
-            className="search col-5"
-            value={speciality}
-            onChange={(e) => {
-              setSpeciality(e.target.value);
-              setSpecialityFilterData(true);
-              setSearchFilterData(false);
-              setSearchName("");
-            }}
-          >
-            <option value="" className="text-secondary">
-              Specialities
+    <div className="justify-center min-h-[calc(100vh-100px)] ml-44">
+      <Filter filterOpen={filterOpen} setFilterOpen={setFilterOpen}>
+        <select
+          id="speciality"
+          name="speciality"
+          className="w-full"
+          value={speciality}
+          onChange={(e) => {
+            setSpeciality(e.target.value);
+            setSpecialityFilterData(true);
+            setSearchFilterData(false);
+            setSearchName("");
+          }}
+        >
+          <option value="" className="text-secondary">
+            Specialities
+          </option>
+          {specialities.map((speciality) => (
+            <option value={speciality._id} key={speciality._id}>
+              {speciality.sub_speciality}
             </option>
-            {specialities.map((speciality) => (
-              <option value={speciality._id} key={speciality._id}>
-                {speciality.sub_speciality}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="col-12 col-md-3 p-2 text-center">
-          <button
-            onClick={() => {
-              window.location.href = "/addfreeLancer";
-            }}
-            className="new-user p-2"
-          >
-            <SiFreelancer className="fs-3" /> Add New FreeLancer
-          </button>
-        </div>
+          ))}
+        </select>
+      </Filter>
+      <div className="flex justify-between items-center my-8">
+        <h1 className="text-2xl">System FreeLancers</h1>
       </div>
 
-      <div className=" w-100 row p-0 m-0 mt-2 justify-content-center">
-        {searchFilterData ? (
-          !searchFilter.length == 0 ? (
-            searchFilter.map((freeLancer) => (
-              <div
-                key={freeLancer._id}
-                className="task-card bg-white  p-2 py-3 row users-data col-11 my-1"
-              >
-                <div className="col-12 fw-bold row text-start">
-                  <div className="col-12 p-2 ">
-                    <SiFreelancer className="fs-1 text-danger" />
-                  </div>
+      <div className="flex items-center justify-between">
+        <input
+          type="search"
+          className="rounded border px-3 py-2 shadow-sm w-1/3"
+          placeholder="Search By Name"
+          value={searchName}
+          onChange={(e) => {
+            setSearchName(e.target.value);
+            setSpecialityFilterData(false);
+            setSearchFilterData(true);
+            setSpeciality("");
+          }}
+        />
+        <button
+          onClick={() => navigate("/addfreeLancer")}
+          className="inline-flex items-center rounded-md border px-3 py-2 text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
+        >
+          <FaPlus className="mr-2" /> Add New Freelancer
+        </button>
+      </div>
 
-                  <p className="col-12 col-sm-6 col-md-5 edit-form-p fw-bold">
-                    {" "}
-                    <span className="edit-form-lable">Name:</span>
-                    <Link
-                      className="text-dark fw-bold"
-                      to={`/freeLancer/${freeLancer._id}`}
-                    >
-                      {freeLancer.freelancername}
-                    </Link>
-                  </p>
-                  <p className="col-12 col-sm-6 col-md-4 edit-form-p ">
-                    <span className="edit-form-lable">Speciality :</span>{" "}
-                    {freeLancer.speciality &&
-                      freeLancer.speciality.sub_speciality}
-                  </p>
-
-                  <p className="col-12 col-sm-6 col-md-3 edit-form-p fw-bold">
-                    {" "}
-                    <span className="edit-form-lable">TaskCount :</span>{" "}
-                    {freeLancer.tasksCount}
-                  </p>
-                  {/*  <p className="col-12 col-sm-6 col-md-4 edit-form-p fw-bold"> <span className="edit-form-lable">CompletedTasks :</span> {freeLancer.completedCount}</p>
-              <p className="col-12 col-sm-6 col-md-4 edit-form-p fw-bold"> <span className="edit-form-lable">TotalGain :</span> {freeLancer.totalGain}</p>
-              <p className="col-12 col-sm-6 col-md-4 edit-form-p fw-bold"> <span className="edit-form-lable">TotalProfit :</span> {freeLancer.totalProfit}</p> */}
-                </div>
-              </div>
-            ))
+      <div className="mt-2">
+        {searchFilterData &&
+          (!searchFilter.length == 0 ? (
+            <table className="min-w-full divide-y divide-gray-200 drop-shadow">
+              <thead className="bg-gray-50 text-cyan-600">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Country
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Task Count
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Completed Tasks
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Total Gain
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Total Profit
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Currency
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {searchFilter.map((freelancer) => (
+                  <tr key={freelancer._id}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <Link
+                        className="text-indigo-600 hover:text-indigo-900 font-medium"
+                        to={`/freelancer/${freelancer._id}`}
+                      >
+                        {freelancer.freelancername}
+                      </Link>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.country.countryName}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.tasksCount}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.completedCount}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.totalGain}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.totalProfit}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.currency && freelancer.currency.currencyname}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           ) : (
             <div className="row  p-3 m-0 text-center">
               <h2>There Is No FreeLancers</h2>
             </div>
-          )
-        ) : (
-          ""
-        )}
-
-        {SpecialityFilterData ? (
-          !SpecialityFilter.length == 0 ? (
-            SpecialityFilter.map((freeLancer) => (
-              <div
-                key={freeLancer._id}
-                className="task-card bg-white  p-2 py-3 row users-data col-11 my-1"
-              >
-                <div className="col-12 fw-bold row text-start">
-                  <p className="col-12 col-sm-6 col-md-4 edit-form-p fw-bold">
-                    {" "}
-                    <span className="edit-form-lable">Name : </span>
-                    <Link
-                      className="text-dark fw-bold"
-                      to={`/freeLancer/${freeLancer._id}`}
-                    >
-                      {freeLancer.freelancername}
-                    </Link>
-                  </p>
-                  <p className="col-12 col-sm-6 col-md-4 edit-form-p ">
-                    <span className="edit-form-lable">Speciality :</span>{" "}
-                    {freeLancer.speciality.sub_speciality}
-                  </p>
-                  <p className="col-12 col-sm-6 col-md-4 edit-form-p fw-bold">
-                    {" "}
-                    <span className="edit-form-lable">TaskCount :</span>{" "}
-                    {freeLancer.tasksCount}
-                  </p>
-                  {/* <p className="col-12 col-sm-6 col-md-4 edit-form-p fw-bold"> <span className="edit-form-lable">CompletedTasks :</span> {freeLancer.completedCount}</p>
-              <p className="col-12 col-sm-6 col-md-4 edit-form-p fw-bold"> <span className="edit-form-lable">TotalCost :</span> {freeLancer.totalGain}</p>
-              <p className="col-12 col-sm-6 col-md-4 edit-form-p fw-bold"> <span className="edit-form-lable">TotalProfit :</span> {freeLancer.totalProfit}</p> */}
-                </div>
-              </div>
-            ))
+          ))}
+        {SpecialityFilterData &&
+          (!SpecialityFilter.length == 0 ? (
+            <table className="min-w-full divide-y divide-gray-200 drop-shadow">
+              <thead className="bg-gray-50 text-cyan-600">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Country
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Task Count
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Completed Tasks
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Total Gain
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Total Profit
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    Currency
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {SpecialityFilter.map((freelancer) => (
+                  <tr key={freelancer._id}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <Link
+                        className="text-indigo-600 hover:text-indigo-900 font-medium"
+                        to={`/freelancer/${freelancer._id}`}
+                      >
+                        {freelancer.freelancername}
+                      </Link>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.country.countryName}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.tasksCount}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.completedCount}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.totalGain}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.totalProfit}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {freelancer.currency && freelancer.currency.currencyname}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           ) : (
             <div className="row  p-3 m-0 text-center">
               <h2>There Is No FreeLancers</h2>
             </div>
-          )
-        ) : (
-          ""
-        )}
+          ))}
       </div>
     </div>
   );
