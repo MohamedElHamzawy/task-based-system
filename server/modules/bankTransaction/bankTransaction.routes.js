1e0f5446e475cd701fdc632c1bbf78defb67784b
Mohamed ElHamzawy <mohamed.elhamzawy.backend@gmail.com>
2024-03-15 16:41:47 +0200

    return bank transactions

--- server/modules/bankTransaction/bankTransaction.routes.js
+++ server/modules/bankTransaction/bankTransaction.routes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const auth = require("../../middleware/auth");
 const {getAllBankTransactions, getBankTransaction, createBankTransaction, deleteBankTransaction} = require("./bankTransaction.controller");
 
-router.get("/", auth(), getAllBankTransactions);
-router.get("/:id", auth(), getBankTransaction);
+router.get("/:id", auth(), getAllBankTransactions);
+// router.get("/:id", auth(), getBankTransaction);
 router.post("/", auth(), createBankTransaction);
 router.delete("/:id", auth(), deleteBankTransaction);
 
