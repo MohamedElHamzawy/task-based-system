cc310e5f243f040eaae8b617431aaf91bc041c92
Ziad Gaafar <ziadgaafar@protonmail.com>
2024-03-15 15:41:02 +0200

    Remove flowbite package and plugin

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -8,9 +8,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("flowbite/plugin")({
-      charts: true,
-    }),
-  ],
+  plugins: [],
 };
