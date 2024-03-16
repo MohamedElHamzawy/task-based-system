a2dd8ed7106bad7d7e941dddb480e5e9fbca3300
Ziad Gaafar <ziadgaafar@protonmail.com>
2024-03-16 15:57:39 +0200

    fixed next() is not defined

--- server/modules/bankTransaction/bankTransaction.controller.js
+++ server/modules/bankTransaction/bankTransaction.controller.js
@@ -3,97 +3,105 @@ const accountModel = require("../../DB/account.model");
 const bankTransactionModel = require("../../DB/bank.transactions.model");
 const HttpError = require("../../common/httpError");
 
-const getAllBankTransactions = async (req, res) => {
-    try {
-        const bankTransactions = await bankTransactionModel.find({$or: [{from: req.params.id}, {to: req.params.id}]});
-        let transactions = [];
-        bankTransactions.forEach(trans => {
-            let getFrom = bankModel.findById(trans.from);
-            if (!getFrom) {
-                getFrom = accountModel.findById(trans.from);
-            }
-            let getTo = bankModel.findById(trans.to);
-            if (!getTo) {
-                getTo = accountModel.findById(trans.to);
-            }
-            let transaction = {
-                id: trans._id,
-                from: getFrom,
-                to: getTo,
-                amount: trans.amount,
-                exchangeRate: trans.exchangeRate,
-                createdAt: trans.createdAt
-            }
-            transactions.push(transaction);
-        });
-        res.status(200).json(transactions);
-    } catch (error) {
-        return next(new HttpError(`Unexpected Error: ${error}`, 500));
-    }
+const getAllBankTransactions = async (req, res, next) => {
+  try {
+    const bankTransactions = await bankTransactionModel.find({
+      $or: [{ from: req.params.id }, { to: req.params.id }],
+    });
+    let transactions = [];
+    bankTransactions.forEach((trans) => {
+      let getFrom = bankModel.findById(trans.from);
+      if (!getFrom) {
+        getFrom = accountModel.findById(trans.from);
+      }
+      let getTo = bankModel.findById(trans.to);
+      if (!getTo) {
+        getTo = accountModel.findById(trans.to);
+      }
+      let transaction = {
+        id: trans._id,
+        from: getFrom,
+        to: getTo,
+        amount: trans.amount,
+        exchangeRate: trans.exchangeRate,
+        createdAt: trans.createdAt,
+      };
+      transactions.push(transaction);
+    });
+    res.status(200).json(transactions);
+  } catch (error) {
+    return next(new HttpError(`Unexpected Error: ${error}`, 500));
+  }
 };
 
-const getBankTransaction = async (req, res) => {
-    try {
-        const bankTransaction = await bankTransactionModel.findById(req.params.id);
-        res.send(bankTransaction);
-    } catch (error) {
-        return next(new HttpError(`Unexpected Error: ${error}`, 500));
-    }
+const getBankTransaction = async (req, res, next) => {
+  try {
+    const bankTransaction = await bankTransactionModel.findById(req.params.id);
+    res.send(bankTransaction);
+  } catch (error) {
+    return next(new HttpError(`Unexpected Error: ${error}`, 500));
+  }
 };
 
-const createBankTransaction = async (req, res) => {
-    try {
-        const bankTransaction = new bankTransactionModel(req.body);
-        await bankTransaction.save();
-        await bankModel.findByIdAndUpdate(
-            bankTransaction.from,
-            { $inc: { balance: -bankTransaction.amount } } //This line is decreasing the balance of the bank that the transaction is from by the amount of the transaction.
-        );
-        await bankModel.findByIdAndUpdate(
-            bankTransaction.to,
-            { $inc: { balance: (bankTransaction.amount * bankTransaction.exchangeRate) } } //This line is increasing the balance of the bank that the transaction is to by the amount of the transaction.
-        );
-        res.status(200).json(bankTransaction);
-    } catch (error) {
-        return next(new HttpError(`Unexpected Error: ${error}`, 500));
-    }
+const createBankTransaction = async (req, res, next) => {
+  try {
+    const bankTransaction = new bankTransactionModel(req.body);
+    await bankTransaction.save();
+    await bankModel.findByIdAndUpdate(
+      bankTransaction.from,
+      { $inc: { balance: -bankTransaction.amount } } //This line is decreasing the balance of the bank that the transaction is from by the amount of the transaction.
+    );
+    await bankModel.findByIdAndUpdate(
+      bankTransaction.to,
+      {
+        $inc: {
+          balance: bankTransaction.amount * bankTransaction.exchangeRate,
+        },
+      } //This line is increasing the balance of the bank that the transaction is to by the amount of the transaction.
+    );
+    res.status(200).json(bankTransaction);
+  } catch (error) {
+    return next(new HttpError(`Unexpected Error: ${error}`, 500));
+  }
 };
 
-const updateBankTransaction = async (req, res) => {
-    try {
-        const bankTransaction = await bankTransactionModel.findByIdAndUpdate(
-            req.params.id,
-            req.body
-        );
-        res.send(bankTransaction);
-    } catch (error) {
-        return next(new HttpError(`Unexpected Error: ${error}`, 500));
-    }
+const updateBankTransaction = async (req, res, next) => {
+  try {
+    const bankTransaction = await bankTransactionModel.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    res.send(bankTransaction);
+  } catch (error) {
+    return next(new HttpError(`Unexpected Error: ${error}`, 500));
+  }
 };
 
-const deleteBankTransaction = async (req, res) => {
-    try {
-        const bankTransaction = await bankTransactionModel.findById(req.params.id);
-        await bankModel.findByIdAndUpdate(
-            bankTransaction.from,
-            { $inc: { balance: bankTransaction.amount } } //This line is increasing the balance of the bank that the transaction is from by the amount of the transaction.
-        );
-        await bankModel.findByIdAndUpdate(
-            bankTransaction.to,
-            { $inc: { balance: -(bankTransaction.amount * bankTransaction.exchangeRate) } } //This line is decreasing the balance of the bank that the transaction is to by the amount of the transaction.
-        );
-        await bankTransactionModel.findByIdAndDelete(
-            req.params.id
-        );
-        res.send(bankTransaction);
-    } catch (error) {
-        return next(new HttpError(`Unexpected Error: ${error}`, 500));
-    }
+const deleteBankTransaction = async (req, res, next) => {
+  try {
+    const bankTransaction = await bankTransactionModel.findById(req.params.id);
+    await bankModel.findByIdAndUpdate(
+      bankTransaction.from,
+      { $inc: { balance: bankTransaction.amount } } //This line is increasing the balance of the bank that the transaction is from by the amount of the transaction.
+    );
+    await bankModel.findByIdAndUpdate(
+      bankTransaction.to,
+      {
+        $inc: {
+          balance: -(bankTransaction.amount * bankTransaction.exchangeRate),
+        },
+      } //This line is decreasing the balance of the bank that the transaction is to by the amount of the transaction.
+    );
+    await bankTransactionModel.findByIdAndDelete(req.params.id);
+    res.send(bankTransaction);
+  } catch (error) {
+    return next(new HttpError(`Unexpected Error: ${error}`, 500));
+  }
 };
 module.exports = {
-    getAllBankTransactions,
-    getBankTransaction,
-    createBankTransaction,
-    updateBankTransaction,
-    deleteBankTransaction
+  getAllBankTransactions,
+  getBankTransaction,
+  createBankTransaction,
+  updateBankTransaction,
+  deleteBankTransaction,
 };
